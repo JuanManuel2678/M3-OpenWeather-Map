e7ceac8b47d7f9ac2d4d0e184522caeb5b6f6111
Juan_manuel <jaredjuan71@gmail.com>
2024-07-01 16:54:38 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold text-[#0955c7] text-center p-4">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
