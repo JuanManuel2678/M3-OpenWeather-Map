bff57b285230008c90f6836c47932f731de93526
JuanManuel2678 <jaredjuan71@gmail.com>
2024-07-06 16:02:26 -0400

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { LeftSide } from "./components/LeftSide";
 import { Main } from "./components/Main";
+import { Modal } from "./components/Modal";
 import { useData } from "./hook/useData";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className="flex flex-wrap w-full h-full min-w-[330px] min-h-screen custon-xs:flex-col  bg-[#100E1D] ">
+      <Modal />
       <LeftSide data={data} gps={gps}/>
       <Main data={data} />
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,43 +1,54 @@
+import lupa from "/public/lupa.svg"
 
+export const Modal = () => {
 
-export const modal = () => {
   return (
-    <aside className="w-[459px] max-h-[1050px] min-h-[800px] h-screen bg-[#1E213A] text-white font-raleway flex flex-col justify-between">
-      
-    <header className="flex justify-between h-[100px]">
+    <div className="w-[459px] h-full max-h-[1050px] min-h-[800px] absolute bg-[#1E213A] text-white font-raleway p-11 gap-6 hidden ">
 
-       <div>
-        <img src="" alt="" />
-        <input type="text" />
-        </div> 
+      <div className='w-full flex justify-end '>
+        <button className=" hover:bg-[#E7E7EB] hover:text-[#555555] active:bg-[#B5B5BB] transition duration-300 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#E7E7EB" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
 
-        <button></button>
-          
-    </header>
+      <header className="flex gap-2 w-[366px] h-[48px] mt-10">
 
-    <div className="group text-white border border-transparent my-2 cursor-pointer p-2 hover:border-[1px] hover:border-white flex justify-between transition-all duration-300" >
-          <p>London</p>
-          <img src="/public/image/vector.svg" alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
+        <div className=" flex gap-2 p-2 w-[268px] h-[48px] border-2 border-[grey]">
+          <img src={lupa} alt="icon lupa" className="w-[24px] h-[24px] top-1/2" />
+          <input type="text" placeholder="Search location" className="bg-[#1E213A] focus:outline-none focus:border-[#E7E7EB] transition duration-300 " />
         </div>
+
+        <button className=" w-[86px] h-[48px] bg-[#3C47E9] font-semibold text-base hover:bg-[#E7E7EB] hover:text-[#555555] active:bg-[#B5B5BB] transition duration-300-">Search</button>
+      </header>
+
+      <div className="flex flex-col gap-4 mt-6">
+
+        <div className="group text-white border border-transparent my-2 cursor-pointer p-2 hover:border-[1px] hover:border-white flex justify-between transition-all duration-300" >
+          <p>London</p>
+          <img src="/public/vector.svg" alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+        </div>
+
         <div className="group text-white border border-transparent my-2 cursor-pointer p-2 hover:border-[1px] hover:border-white flex justify-between transition-all duration-300" >
           <p>Barcelona</p>
-          <img src="/public/image/vector.svg" alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
+          <img src="/public/vector.svg" alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
         </div>
+
         <div className="group text-white border border-transparent my-2 cursor-pointer p-2 hover:border-[1px] hover:border-white flex justify-between transition-all duration-300"  >
           <p>Long Beach</p>
-          <img src={vector} alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
+          <img src="/public/vector.svg" alt="vector" className="w-5 h-5 relative bg-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
         </div>
 
-    
-    
+      </div>
 
-  </aside>
+    </div>
   )
 }
 
-export default modal
+export default Modal
 
 
 // onClick={() => handleSearch('London')}
-// onClick={() => handleSearch('Barcelona')} 
+// onClick={() => handleSearch('Barcelona')}
 // onClick={() => handleSearch('Long Beach')}
